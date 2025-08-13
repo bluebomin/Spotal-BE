cfb3868e8f900922dc8da95998b91978b3f43878
seoah <24seoah@sookmyung.ac.kr>
2025-08-13 15:53:54 +0900

    Feat : 감정, 위치 태그 추가

---
+++ community/management/commands/load_initial_data.py
@@ -0,0 +1,27 @@
+import os
+import csv
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from community.models import emotion, location  # 모델 경로 맞게 수정
+
+class Command(BaseCommand):
+    help = 'Load initial emotions and locations from CSV files'
+
+    def handle(self, *args, **kwargs):
+        # CSV 경로 지정
+        emotion_csv_path = os.path.join(settings.BASE_DIR, 'data', 'emotion.csv')
+        location_csv_path = os.path.join(settings.BASE_DIR, 'data', 'location.csv')
+
+        # 감정 데이터 로드
+        with open(emotion_csv_path, newline='', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                emotion.objects.get_or_create(emotion_id=row['emotion_id'], name=row['name'])
+
+        # 위치 데이터 로드
+        with open(location_csv_path, newline='', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                location.objects.get_or_create(location_id=row['location_id'], name=row['name'])
+
+        self.stdout.write(self.style.SUCCESS('CSV 데이터 로드 완료'))
--- community/models.py
+++ community/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+# 커뮤니티 글
+class memory(models.Model):
+    memory_id = models.AutoField(primary_key=True)
+    emotion_id = models.IntegerField()
+    location_id = models.IntegerField()
+    user_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+# 커뮤니티 이미지
+class image(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    memory_id = models.ForeignKey(memory, on_delete=models.CASCADE, related_name='images')
+    image_url = models.URLField(max_length=200)
+    image_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.image_name
+
+class emotion(models.Model):
+    emotion_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class location(models.Model):
+    location_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+   
