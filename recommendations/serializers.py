75a9ca891abb26fea35bff53b3eb22812b2a724c
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-18 20:32:17 +0900

    chore/ai 요약관련 로직 수정, user 로그인 권한 필요없도록 SavedPlace 시리얼라이저 수정

--- recommendations/serializers.py
+++ recommendations/serializers.py
@@ -2,10 +2,19 @@ from rest_framework import serializers
 from .models import Place, AISummary, SavedPlace
 
 
+# AI 요약 정보
+class AISummarySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AISummary
+        fields = ("summary_id", "summary")
+        read_only_fields = ("summary_id",)
+
 # AI 추천 가게 정보
 class PlaceSerializer(serializers.ModelSerializer):
     emotion_name = serializers.CharField(source="emotion.name", read_only=True)
     location_name = serializers.CharField(source="location.name", read_only=True)
+    ai_summary = AISummarySerializer(many=True, read_only=True)
 
     class Meta:
         model = Place
@@ -17,26 +26,22 @@ class PlaceSerializer(serializers.ModelSerializer):
             "emotion_name",  # name (정겨움, 세심함 등)
             "location",      # id
             "location_name", # name (효창동, 후암동 등)
+            "ai_summary",
             "created_date",
             "modified_date",
         )
         read_only_fields = ("shop_id", "created_date", "modified_date")
 
+    def get_ai_summary(self, obj):
+        # 여기서 AI 요약 생성 로직 호출 or 저장된 요약 리턴
+        return getattr(obj, 'ai_summary', None)
 
-# AI 요약 정보
-class AISummarySerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = AISummary
-        fields = ("summary_id", "shop", "summary")
-        read_only_fields = ("summary_id")
 
 
 # 사용자가 보관한 추천 가게
 class SavedPlaceSerializer(serializers.ModelSerializer):
-    shop = PlaceSerializer(read_only=True)  # 가게 정보도 같이 보여줌 
-
     class Meta:
         model = SavedPlace
-        fields = ("saved_id", "shop", "created_date")
-        read_only_fields = ("saved_id", "created_date")
+        fields = ("saved_id", "shop", "user", "created_date")
+        read_only_fields = ("saved_id", "created_date") # user는 body에서 직접 넘김ㅋㅋ 
