1a38911bd01ed1216834c45e0cd48f1931470111
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-22 21:49:10 +0900

    feat/users 모델에 default 설명값 추가

--- users/models.py
+++ users/models.py
@@ -29,7 +29,8 @@ class User(AbstractUser):
     username = models.CharField(max_length=150, unique=False, blank=True, null=True) 
     email = models.EmailField(unique=True, verbose_name='이메일')
     nickname = models.CharField(max_length=255, verbose_name='닉네임')
-    detail = models.CharField(max_length=255, blank=True, verbose_name='회원설명')
+    detail = models.CharField(max_length=255, blank=True, verbose_name='회원설명',
+                              default="따뜻함을 좋아하는 감성탐험가")
     
     # username 필드 대신 email을 사용자 식별자로 사용
     USERNAME_FIELD = 'email'
