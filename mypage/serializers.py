57890cac8168ab4b4e5c2ca6e5fc2cc897695d0c
minnnnnnni <dkstodtjdals@gmail.com>
2025-08-22 21:16:51 +0900

    feat/감정보관함에 보관한 장소만 가져오도록 SavedPlaceSerializer 수정

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from .models import User, Bookmark, SavedPlace, Community, Place, Image
+from community.models import Bookmark, Memory, Image
+from recommendations.models import SavedPlace
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,31 +12,40 @@ class UserSerializer(serializers.ModelSerializer):
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ["image_id", "image_url", "image_name"]
+        fields = ["image_url"]
 
 class CommunitySummarySerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True) 
     class Meta:
-        model = Community
-        fields = ["id", "images"]
+        model = Memory
+        fields = ["images"]
 
 class BookmarkSerializer(serializers.ModelSerializer):
     post = CommunitySummarySerializer(source="memory") 
     class Meta:
         model = Bookmark
-        fields = ["id", "created_date", "post"]
+        fields = ["created_date", "post"]
 
-'''
-class PlaceSummarySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Place
-        fields = ["id", "name", "status", "address", "summary", "image"]
+
 
 class SavedPlaceSerializer(serializers.ModelSerializer):
-    shop = PlaceSummarySerializer()
+    name = serializers.CharField(source="shop.name", read_only=True)
+    address = serializers.CharField(source="shop.address", read_only=True)
+    image_url = serializers.CharField(source="shop.image_url", read_only=True)
+    emotions = serializers.SlugRelatedField(
+        source="shop.emotions",
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+    summary = serializers.CharField(source="summary_snapshot", read_only=True)
 
     class Meta:
         model = SavedPlace
-        fields = ["id", "shop"]
-
-'''
+        fields = [
+            "name",
+            "address",
+            "image_url",
+            "emotions",
+            "summary",
+        ]
